6e50c6db536dd5a7182bca5fc9a770c92488a078
DESKTOP-35LQ308\lucas <lmiller@universityhighschool.org>
2021-01-06 11:14:01 -0500

    Day One Stuff

---
+++ Player/Player.gd
@@ -0,0 +1,14 @@
+extends RigidBody
+
+export var speed := .05
+var velocity : Vector3 
+
+func _physics_process(delta):
+	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
+	
+	apply_central_impulse(velocity.normalized() * speed)
+	
+
+func _ready():
+	pass
